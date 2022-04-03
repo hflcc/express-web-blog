a2a0d3d522eb1b1a09218c6535cc5add57088a55
hingbelieve@163.com <Hingbelieve@163.com>
2022-04-04 00:00:26 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'semi': ['error', 'always'],
+		'quotes': ['error', 'single'],
+		'no-multi-spaces': 'error'
+	}
+};
